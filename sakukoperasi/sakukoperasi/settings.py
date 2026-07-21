714c2b7abb187c26a3df5be56684137cf8dae9bf
tahutech <nuralisholikhin19@gmail.com>
2026-07-21 22:30:16 +0700

    renamin from member to core

--- sakukoperasi/sakukoperasi/settings.py
+++ sakukoperasi/sakukoperasi/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'member',
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
--- sakukoperasi/sakukoperasi/urls.py
+++ sakukoperasi/sakukoperasi/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from member.views import MemberViewSet
+from core.views import MemberViewSet
 
 router = DefaultRouter()
 router.register(r'members', MemberViewSet)
